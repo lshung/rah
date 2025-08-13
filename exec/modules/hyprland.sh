4ad4c8b87b8f08edfdf43861521dd279b2416303
Hung <ls@hung.vn>
2025-08-13 11:10:08 +0700

    Add other colors for hyprlock

--- exec/modules/hyprland.sh
+++ exec/modules/hyprland.sh
@@ -10,8 +10,21 @@ fi
 set -e
 
 echo "Cập nhật cấu hình Hyprland..."
-mkdir -p "$HOME"/.config/hypr
-rm -rf "$HOME"/.config/hypr/*
-cp -r "$CONFIGS_DIR"/hypr/* "$HOME"/.config/hypr/
+
+# Khai báo biến
+HYPR_CONFIG_DIR="$HOME"/.config/hypr
+HYPRLOCK_CONFIG_FILE="$HYPR_CONFIG_DIR"/hyprlock.conf
+
+# Dọn dẹp thư mục config của Hyprland
+mkdir -p "$HYPR_CONFIG_DIR"
+rm -rf "$HYPR_CONFIG_DIR"/*
+
+# Sao chép template cấu hình Hyprland
+cp -r "$CONFIGS_DIR"/hypr/* "$HYPR_CONFIG_DIR"/
+
+# Chỉnh sửa cấu hình hyprlock theo theme, flavor và accent
+sed -i "s/@@theme@@/$THEME_NAME/g" "$HYPRLOCK_CONFIG_FILE"
+sed -i "s/@@flavor@@/$THEME_FLAVOR/g" "$HYPRLOCK_CONFIG_FILE"
+sed -i "s/@@accent@@/$THEME_ACCENT/g" "$HYPRLOCK_CONFIG_FILE"
 
 hyprctl reload
