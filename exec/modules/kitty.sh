027cd8f265f3a358878cd5b6d6608b3176322edf
Hung <ls@hung.vn>
2025-08-13 08:46:11 +0700

    Add other themes for kitty

--- exec/modules/kitty.sh
+++ exec/modules/kitty.sh
@@ -10,6 +10,19 @@ fi
 set -e
 
 echo "Cập nhật cấu hình Kitty..."
-mkdir -p "$HOME"/.config/kitty
-rm -rf "$HOME"/.config/kitty/*
-cp -r "$CONFIGS_DIR"/kitty/* "$HOME"/.config/kitty/
+
+# Khai báo biến
+KITTY_CONFIG_DIR="$HOME"/.config/kitty
+
+# Dọn dẹp thư mục config của Kitty
+mkdir -p "$KITTY_CONFIG_DIR"
+rm -rf "$KITTY_CONFIG_DIR"/*
+mkdir -p "$KITTY_CONFIG_DIR"/colors
+
+# Sao chép template cấu hình Kitty
+cp "$CONFIGS_DIR/kitty/kitty.conf" "$KITTY_CONFIG_DIR"/kitty.conf
+cp "$CONFIGS_DIR/kitty/colors/$THEME_NAME-$THEME_FLAVOR.conf" "$KITTY_CONFIG_DIR"/colors/
+
+# Chỉnh sửa cấu hình Kitty theo theme và flavor
+sed -i "s/@@theme@@/$THEME_NAME/g" "$KITTY_CONFIG_DIR"/kitty.conf
+sed -i "s/@@flavor@@/$THEME_FLAVOR/g" "$KITTY_CONFIG_DIR"/kitty.conf
