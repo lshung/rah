25198d7b8dac2d9517f5fd4c2604f758d3ff9341
Hung <ls@hung.vn>
2025-08-23 20:25:31 +0700

    Separate Hyprland config into multiple files

--- setup/modules/hyprland.sh
+++ setup/modules/hyprland.sh
@@ -19,9 +19,15 @@ HYPRLOCK_CONFIG_FILE="$HYPR_CONFIG_DIR"/hyprlock.conf
 mkdir -p "$HYPR_CONFIG_DIR"
 sync_contents_of_two_dirs "$APP_CONFIGS_HYPR_DIR" "$HYPR_CONFIG_DIR"
 
+echo "Editing Hyprland variables configuration..."
+sed -i "s/^\$COLOR_THEME = .*$/\$COLOR_THEME = ${THEME_NAME}-${THEME_FLAVOR}/g" "$HYPR_CONFIG_DIR"/hyprland/variables.conf
+sed -i "s/^\$ANIMATIONS_STYLE = .*$/\$ANIMATIONS_STYLE = ${HYPRLAND_ANIMATIONS_STYLE}/g" "$HYPR_CONFIG_DIR"/hyprland/variables.conf
+
 # Edit hyprlock configuration according to theme, flavor and accent
+echo "Editing hyprlock configuration..."
 sed -i "s/@@theme@@/$THEME_NAME/g" "$HYPRLOCK_CONFIG_FILE"
 sed -i "s/@@flavor@@/$THEME_FLAVOR/g" "$HYPRLOCK_CONFIG_FILE"
 sed -i "s/@@accent@@/$THEME_ACCENT/g" "$HYPRLOCK_CONFIG_FILE"
 
+echo "Reloading Hyprland..."
 hyprctl reload
