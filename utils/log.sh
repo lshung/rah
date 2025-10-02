464060e37901597a7ae169947a6ae32852a8dae4
Hung <ls@hung.vn>
2025-10-02 12:57:56 +0700

    Modify log util

--- utils/log.sh
+++ utils/log.sh
@@ -1,71 +1,66 @@
 #!/bin/bash
 
-# Exit if this script is being executed directly
 [[ "${BASH_SOURCE[0]}" != "${0}" ]] || { echo -e "[\033[31m ERRO \033[0m] This script cannot be executed directly." 1>&2; exit 1; }
 
 set -euo pipefail
 
-readonly NC='\033[0m'        # Reset color
-readonly RED='\033[0;31m'    # Red
-readonly GREEN='\033[0;32m'  # Green
-readonly YELLOW='\033[0;33m' # Yellow
+readonly NC='\033[0m'           # Reset color
+readonly RED='\033[0;31m'       # Red
+readonly GREEN='\033[0;32m'     # Green
+readonly YELLOW='\033[0;33m'    # Yellow
 
 log_message() {
     local level="$1"
     local message="$2"
     local include_tee="${3:-yes}"
-    local timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+
+    if [[ "$LOG_TIMESTAMP_INCLUDED" == "yes" ]]; then
+        local timestamp="[$(date '+%Y-%m-%d %H:%M:%S')] "
+    else
+        local timestamp=""
+    fi
 
     case "$level" in
         "EMPTY")
             if [[ "$include_tee" == "no" ]]; then
                 echo ""
             else
-                echo "" | tee -a "$APP_LOG_FILE"
+                echo "" | tee -a "$LOG_FILE"
             fi
             ;;
         "INFO")
-            level_text=" INFO "
             if [[ "$include_tee" == "no" ]]; then
-                echo "[$timestamp] [${level_text}] $message"
+                echo "$timestamp[ INFO ] $message"
             else
-                echo "[$timestamp] [${level_text}] $message" | tee -a "$APP_LOG_FILE"
+                echo "$timestamp[ INFO ] $message" | tee -a "$LOG_FILE"
             fi
             ;;
         "WARNING")
-            level_color="$YELLOW"
-            level_text=" WARN "
             if [[ "$include_tee" == "no" ]]; then
-                echo -e "[$timestamp] [${level_color}${level_text}${NC}] $message" 1>&2
+                echo -e "$timestamp[${YELLOW} WARN ${NC}] $message" 1>&2
             else
-                echo -e "[$timestamp] [${level_color}${level_text}${NC}] $message" | tee -a "$APP_LOG_FILE" 1>&2
+                echo -e "$timestamp[${YELLOW} WARN ${NC}] $message" | tee -a "$LOG_FILE" 1>&2
             fi
             ;;
         "ERROR")
-            level_color="$RED"
-            level_text=" ERRO "
             if [[ "$include_tee" == "no" ]]; then
-                echo -e "[$timestamp] [${level_color}${level_text}${NC}] $message" 1>&2
+                echo -e "$timestamp[${RED} ERRO ${NC}] $message" 1>&2
             else
-                echo -e "[$timestamp] [${level_color}${level_text}${NC}] $message" | tee -a "$APP_LOG_FILE" 1>&2
+                echo -e "$timestamp[${RED} ERRO ${NC}] $message" | tee -a "$LOG_FILE" 1>&2
             fi
             ;;
         "OK")
-            level_color="$GREEN"
-            level_text="  OK  "
             if [[ "$include_tee" == "no" ]]; then
-                echo -e "[$timestamp] [${level_color}${level_text}${NC}] $message"
+                echo -e "$timestamp[${GREEN}  OK  ${NC}] $message"
             else
-                echo -e "[$timestamp] [${level_color}${level_text}${NC}] $message" | tee -a "$APP_LOG_FILE"
+                echo -e "$timestamp[${GREEN}  OK  ${NC}] $message" | tee -a "$LOG_FILE"
             fi
             ;;
         "FAILED")
-            level_color="$RED"
-            level_text="FAILED"
             if [[ "$include_tee" == "no" ]]; then
-                echo -e "[$timestamp] [${level_color}${level_text}${NC}] $message" 1>&2
+                echo -e "$timestamp[${RED}FAILED${NC}] $message" 1>&2
             else
-                echo -e "[$timestamp] [${level_color}${level_text}${NC}] $message" | tee -a "$APP_LOG_FILE" 1>&2
+                echo -e "$timestamp[${RED}FAILED${NC}] $message" | tee -a "$LOG_FILE" 1>&2
             fi
             ;;
     esac
@@ -96,11 +91,11 @@ log_failed() {
 }
 
 util_clean_up_log_files() {
-    local log_files_list=($(find "$APP_LOG_DIR" -name "*.log" -type f -printf '%T@ %p\n' 2>/dev/null | sort -nr | cut -d' ' -f2-))
+    local log_files_list=($(find "$LOG_DIR" -name "*.log" -type f -printf '%T@ %p\n' 2>/dev/null | sort -nr | cut -d' ' -f2-))
     local log_files_count=${#log_files_list[@]}
 
-    if [[ $log_files_count -gt $APP_LOG_FILE_COUNT ]]; then
-        for ((i = APP_LOG_FILE_COUNT; i < log_files_count; i++)); do
+    if [[ $log_files_count -gt $LOG_FILE_COUNT ]]; then
+        for ((i = LOG_FILE_COUNT; i < log_files_count; i++)); do
             [ -f "${log_files_list[$i]}" ] && rm -f "${log_files_list[$i]}"
         done
     fi
